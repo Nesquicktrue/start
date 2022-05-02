578b486e885b90c26c681b37f8facad3ee433515
Petr Martan <nesquick.true@gmail.com>
2022-05-02 08:33:43 +0200

    Pokus

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -2,18 +2,32 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class FirstController {
-    #[Route('/')]
+class FirstController extends AbstractController {
+
+    #[Route('/' , name: 'app_home')]
     public function homepage(): Response
     {
-        return new Response("Homepage");
-    }
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
 
-    #[Route('/browse/{slug}')]
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'První vinyl',
+            'tracks' => $tracks
+        ]);
+    }
+    
+    #[Route('/browse/{slug}', name: 'app_browse')]
     public function browse(string $slug = null): Response
     {
         if ($slug) {
@@ -23,7 +37,9 @@ class FirstController {
             $title = "All genres";    
         }
 
-    return new Response("Browse: " .$title);
+        return $this->render('vinyl/browse.html.twig', [
+        'title' => $title,
+        ]);
 
     }
 }
